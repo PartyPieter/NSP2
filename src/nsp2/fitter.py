3f64ff9201ecc3cd98bb7c05a1ec8f227f852c59
Part pieter <manusvanderham@outlook.com>
2024-11-19 17:40:29 +0100

    made the script working, added working fit function

--- src/nsp2/fitter.py
+++ src/nsp2/fitter.py
@@ -4,57 +4,72 @@ import pandas as pd
 from lmfit import models
 
 begin_t = 0.0041
+Temperatures = [41, 36]
 x = 0.06
 v = []
+v_err = []
 df = []
-#df = pd.read_csv("real_test_meting.csv", sep = "\t")
 
+cut_value = 3
 
-#V_error = []
-
-#df["V_err"] = df["V"] * 0.001 + 0.044
-cut_value = 2
-cut_values = []
-
+# executing speed calculations for every measurement
 aantal_metingen = 2
-
 for i in range(aantal_metingen):
-
+   
+    cut_values = []
     df = pd.read_csv("meting_" + str(i) + ".csv", sep = "\t")
     time = df.iloc[:,0]
     Amplitude = df.iloc[:,1]
+    avg_amp = sum(Amplitude)/len(Amplitude)
 
     for i in range (len(Amplitude) - 1):
-        delta_amp = np.abs(Amplitude[i + 1] - Amplitude[i])
-        print(delta_amp)
-        if delta_amp > cut_value and time[i] > 0.02:
+        
+        # save points where amplitude is higher then average + cutvalue
+        if Amplitude[i] > avg_amp + cut_value and time[i] > begin_t:
          cut_values.append(i)
 
+    # time it takes for wave to move is time of first point where amplitude is high enough - time start of measurement
     t  = time[cut_values[0]] - begin_t
+
     print(t)
     print(cut_values[0])
     v.append(2 * x / t)
+    #TO BE ISSUED
+    v_err.append(3)
+
+    # plot time-amplitude curve
     plt.plot(time, Amplitude)
     plt.plot(time[cut_values[0]], Amplitude[cut_values[0]], 'ro', markersize = 10)
+    plt.xlabel("t (ms) ")
+    plt.ylabel("Amp")
+    plt.title("time - amplitude curve")
     plt.show()
 
 print(v)
 
+# plotting V-T plot
+plt.plot(Temperatures, v, 'bo')
+plt.xlabel("Temperature (C) ")
+plt.ylabel("Speed (m/s)")
+plt.title("Speed of sound through water at given temperature ")
+plt.show()
 
-"""
-def V_fit_func(T, p1, p2, p3):
+# TO BE PERFECTED
+def V_fit_func(T, p1, p3):
   #  V = p1**p2 * T**p3
-    V = p1  + (p2 * T) ** p3
-    return V 
+    v = p1  +  p3 *( T)  
+    return v 
 
 print("yoyoyo")
 
+# fitting in motion
 V_curve_model = models.Model(V_fit_func)
 
-V_fit_result = V_curve_model.fit(df["V"], T=df["T"], weights = 1 / df["V_err"], p1 = 1, p2 = 2, p3 = -0.5)
+weights = np.array([1 / err for err in v_err])
+V_fit_result = V_curve_model.fit(v, T=Temperatures, weights = weights , p1 = 1, p3 = -0.5)
 
-
+# print and plot fit
 V_fit_result.plot(numpoints = 1000)
+plt.ylim(1.6 , 1.7)
 plt.show()
-V_fit_result
-"""
+print(V_fit_result.fit_report())
